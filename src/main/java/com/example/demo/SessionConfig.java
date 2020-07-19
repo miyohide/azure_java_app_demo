8d033724756b40e83ba858066b555e581016f001
Hidenori Miyoshi <miyohide@gmail.com>
2020-07-19 18:14:03 +0900

    store JSON to Redis.

--- src/main/java/com/example/demo/SessionConfig.java
+++ src/main/java/com/example/demo/SessionConfig.java
@@ -1,10 +1,17 @@
 package com.example.demo;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
 
 @Configuration
 @EnableRedisHttpSession
 public class SessionConfig extends AbstractHttpSessionApplicationInitializer {
+    @Bean
+    public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
+        return new GenericJackson2JsonRedisSerializer();
+    }
 }
