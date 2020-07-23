63f7ff841e9bef67c0e0a066f62fa628f743c95b
Hidenori Miyoshi <miyohide@gmail.com>
2020-07-23 16:09:33 +0900

    fix error.

--- src/main/java/com/example/demo/SessionConfig.java
+++ src/main/java/com/example/demo/SessionConfig.java
@@ -2,8 +2,12 @@ package com.example.demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.session.web.context.AbstractHttpSessionApplicationInitializer;
 
@@ -13,5 +17,19 @@ public class SessionConfig extends AbstractHttpSessionApplicationInitializer {
     @Bean
     public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
         return new GenericJackson2JsonRedisSerializer();
+    }
+    @Bean
+    public static ConfigureRedisAction configureRedisAction() {
+        return ConfigureRedisAction.NO_OP;
+    }
+
+    @Bean
+    public LettuceConnectionFactory connectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(System.getenv("REDIS_HOST"));
+        redisStandaloneConfiguration.setPassword(System.getenv("REDIS_PASSWORD"));
+        redisStandaloneConfiguration.setPort(Integer.parseInt(System.getenv("REDIS_PORT")));
+        LettuceClientConfiguration lettuceClientConfiguration = LettuceClientConfiguration.builder().useSsl().build();
+        return new LettuceConnectionFactory(redisStandaloneConfiguration, lettuceClientConfiguration);
     }
 }
