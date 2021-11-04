139b9814203c7ba029163e7af5df00b18915a270
Hidenori Miyoshi <miyohide@gmail.com>
2021-11-04 20:36:43 +0900

    delete postgresql and redis

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -13,30 +13,9 @@ import java.util.concurrent.atomic.AtomicLong;
 public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
-    private final SessionInfo sessionInfo;
-
-    public GreetingController(SessionInfo sessionInfo) {
-        this.sessionInfo = sessionInfo;
-    }
 
     @GetMapping("/")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        if (sessionInfo.getName() == null) {
-            sessionInfo.setId(counter.incrementAndGet());
-            sessionInfo.setName(name);
-            sessionInfo.setCreatedAt(new Date());
-        }
-        return new Greeting(
-                sessionInfo.getId(),
-                String.format(template, sessionInfo.getName()),
-                sessionInfo.getCreatedAt()
-                );
-    }
-
-    @GetMapping("/goodbye")
-    public String goodbye(HttpSession session) {
-        Optional<String> name = Optional.ofNullable(sessionInfo.getName());
-        session.invalidate();
-        return "Goodbye " + name.orElse("anonymous");
+        return new Greeting(counter.incrementAndGet(), String.format(template, name), new Date());
     }
 }
