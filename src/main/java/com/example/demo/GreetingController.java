49b67b53fc8386a79507e513c9e495502b7a6d34
Hidenori Miyoshi <miyohide@gmail.com>
2021-11-04 20:37:08 +0900

    delete unused import statement

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -4,9 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpSession;
 import java.util.Date;
-import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
