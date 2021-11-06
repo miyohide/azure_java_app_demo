25f0abe0b895948f6de18acc52988fbe64b2d8b2
Hidenori Miyoshi <miyohide@gmail.com>
2021-11-06 17:54:38 +0900

    generate many objects for debug2.

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -18,9 +18,15 @@ public class GreetingController {
     @GetMapping("/")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
         logger.info("----- start GreetingController#greeting");
-        for (int i = 0; i < 100_000; i++) {
-            Greeting g = new Greeting(1, "hogehoge", new Date());
-            logger.info(g.toString());
+        String s1 = new String();
+        String s2 = new String();
+
+        for (int i = 0; i < 1_000; i++) {
+            s1 = s1.concat("a");
+        }
+        for (int i = 0; i < 1_000; i++) {
+            s2 = s2.concat(s2);
+            logger.info("-----" + s2);
         }
         return new Greeting(counter.incrementAndGet(), String.format(template, name), new Date());
     }
