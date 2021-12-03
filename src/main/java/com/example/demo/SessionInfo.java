ef846131d407e24ab0159d6015a9e48d4fe9bc5d
Hidenori Miyoshi <miyohide@gmail.com>
2021-12-03 21:35:39 +0900

    add sessionInfo class for save/get session data

---
+++ src/main/java/com/example/demo/SessionInfo.java
@@ -0,0 +1,37 @@
+package com.example.demo;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class SessionInfo implements Serializable {
+  private static final long serialVersionUID = 1L;
+
+  private long id;
+  private String name;
+  private Date createdAt;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Date getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(Date createdAt) {
+    this.createdAt = createdAt;
+  }
+
+}
