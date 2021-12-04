a73925c280df30488513feaac624ec0cd918fa24
Hidenori Miyoshi <miyohide@gmail.com>
2021-12-04 17:49:34 +0900

    update controller using session

--- src/main/java/com/example/demo/GreetingController.java
+++ src/main/java/com/example/demo/GreetingController.java
@@ -1,33 +1,40 @@
 package com.example.demo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
+import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 public class GreetingController {
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
-    private static final Logger logger = LoggerFactory.getLogger(GreetingController.class);
+    private final SessionInfo sessionInfo;
+
+    public GreetingController(SessionInfo sessionInfo) {
+        this.sessionInfo = sessionInfo;
+    }
 
     @GetMapping("/")
     public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        logger.info("----- start GreetingController#greeting");
-//        String s1 = new String();
-//        String s2 = new String();
-//
-//        for (int i = 0; i < 100; i++) {
-//            s1 = s1.concat("a");
-//        }
-//        for (int i = 0; i < 100; i++) {
-//            s2 = s2.concat(s2);
-//            logger.info("-----" + s2);
-//        }
-        return new Greeting(counter.incrementAndGet(), String.format(template, name), new Date());
+        if (sessionInfo.getName() == null) {
+            sessionInfo.setId(counter.incrementAndGet());
+            sessionInfo.setName(name);
+            sessionInfo.setCreatedAt(new Date());
+        }
+        return new Greeting(sessionInfo.getId(), String.format(template, sessionInfo.getName()),
+                sessionInfo.getCreatedAt());
+    }
+
+    @GetMapping("/goodbye")
+    public String goodbye(HttpSession session) {
+        Optional<String> name = Optional.ofNullable(sessionInfo.getName());
+        session.invalidate();
+        return "Goodbye " + name.orElse("anonymous");
     }
 }
