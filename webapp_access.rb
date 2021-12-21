2c7e6b715c5000a074d5f0e375145f9daed226d2
Hidenori Miyoshi <miyohide@gmail.com>
2021-12-21 21:09:16 +0900

    set cookie data

--- webapp_access.rb
+++ webapp_access.rb
@@ -1,8 +1,20 @@
 require "net/http"
 require "json"
 
-uri = URI.parse("http://localhost:8080/")
+# すでにあるセッションを削除
+uri = URI.parse("http://localhost:8080/goodbye")
+Net::HTTP.get_response(uri)
+
+# セッションを作成
+uri = URI.parse("http://localhost:8080/?name=1234567890")
 response = Net::HTTP.get_response(uri)
+session_value = response['set-cookie'].split(';')[0]
+
+# セッション付きで送付
+uri = URI.parse("http://localhost:8080/")
+http = Net::HTTP.new(uri.host, uri.port)
+headers = { "Cookie" => session_value }
+response = http.get(uri.path, headers)
 
 body = JSON.parse(response.body, symbolize_names: true)
 
