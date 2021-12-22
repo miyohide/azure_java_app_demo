732a0396b74697898fd11cd95743c23813670f40
Hidenori Miyoshi <miyohide@gmail.com>
2021-12-22 20:25:07 +0900

    refactoring. define constant

--- webapp_access.rb
+++ webapp_access.rb
@@ -1,20 +1,19 @@
 require "net/http"
 require "json"
 
+ACCESS_HOST = "localhost"
+ACCESS_PORT = 8080
+
+http = Net::HTTP.new(ACCESS_HOST, ACCESS_PORT)
+
 # すでにあるセッションを削除
-uri = URI.parse("http://localhost:8080/goodbye")
-Net::HTTP.get_response(uri)
-
+http.get("/goodbye")
 # セッションを作成
-uri = URI.parse("http://localhost:8080/?name=1234567890")
-response = Net::HTTP.get_response(uri)
+response = http.get("/?name=1234567890")
 session_value = response['set-cookie'].split(';')[0]
-
 # セッション付きで送付
-uri = URI.parse("http://localhost:8080/")
-http = Net::HTTP.new(uri.host, uri.port)
 headers = { "Cookie" => session_value }
-response = http.get(uri.path, headers)
+response = http.get("/", headers)
 
 body = JSON.parse(response.body, symbolize_names: true)
 
